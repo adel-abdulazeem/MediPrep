4d15d94482e559a0098d449c439c6dcbe53acc47
Adel Abdulazeem <adelnasr029@gmail.com>
2025-03-10 06:04:30 +0200

    configured auth

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -15,41 +15,54 @@ exports.getLogin = (req, res) => {
 }
 };
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
   const validationErrors = [];
-  if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
-  if (validator.isEmpty(req.body.password))
-    validationErrors.push({ msg: "Password cannot be blank." });
-
-  if (validationErrors.length) {
-    req.flash("errors", validationErrors);
-    return  res.status(400).json({ 
-            error: error.message,
-            details: error.errors ? Object.values(error.errors).map(err => err.message) : []
+  try{
+    if (!validator.isEmail(req.body.email))
+      validationErrors.push({ msg: "Please enter a valid email address." });
+    if (validator.isEmpty(req.body.password))
+      validationErrors.push({ msg: "Password cannot be blank." });
+  
+    if (validationErrors.length) {
+      req.flash("errors", validationErrors);
+      return  res.status(400).json({ 
+              error: error.message,
+              details: error.errors ? Object.values(error.errors).map(err => err.message) : []
+      });
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, {
+      gmail_remove_dots: false,
     });
-  }
-  req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
-
-  passport.authenticate("local", (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      req.flash("errors", info);
-      return res.status(400).json();
-    }
-    req.logIn(user, (err) => {
+  
+    passport.authenticate("local", (err, user, info) => {
       if (err) {
         return next(err);
       }
-      console.log()
-      req.flash("success", { msg: "Success! You are logged in." });
-      res.status(200).json({userName: req.user.userName, role: req.user.role});
-    });
-  })(req, res, next);
+      if (!user) {
+        req.flash("errors", info);
+        return res.status(400).json();
+      }
+      req.logIn(user, async (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", { msg: "Success! You are logged in." });
+        const user = await User.findOne({ email: req.body.email });
+
+        res.json({
+          success: true,
+          user: {
+            id: user._id,
+            userName: user.userName,
+            email: user.email,
+            role: user.role
+          },
+        });
+        });
+    })(req, res, next);
+  } catch(error){
+    console.log(error)
+  }
 };
 
 exports.logout = (req, res) => {
@@ -72,7 +85,8 @@ exports.getSignup = (req, res) => {
    res.status(400).json();
 };
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
+  try {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
@@ -97,30 +111,38 @@ exports.postSignup = (req, res, next) => {
     password: req.body.password,
     role: req.body.role
   });
-
-  User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-    (err, existingUser) => {
-      if (err) {
-        return next(err);
-      }
+      // Updated findOne usage with async/await
+      const existingUser = await User.findOne({
+        $or: [{ email: req.body.email }, { userName: req.body.userName }]
+      });
+  
       if (existingUser) {
         req.flash("errors", {
           msg: "Account with that email address or username already exists.",
         });
-        return res.status(400).json('cannot sign up');
+        return res.status(400).json('Cannot sign up');
       }
-      user.save((err) => {
-        if (err) {
-          return next(err);
-        }
+
+      // Save user with async/await
+      await user.save();
+  
+      // Wrap req.logIn in a Promise
+      await new Promise((resolve, reject) => {
         req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
-          res.status(200).json('alread existed');
+          if (err) return reject(err);
+          resolve();
         });
       });
+      res.json({
+        success: true,
+        user: {
+          id: user._id,
+          userName: user.userName,
+          email: user.email,
+          role: user.role
+        }
+      });  
+    } catch (err) {
+      next(err);
     }
-  );
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -41,14 +41,9 @@ UserSchema.pre("save", function save(next) {
 });
 
 // Helper method for validating user's password.
-
-UserSchema.methods.comparePassword = function comparePassword(
-  candidatePassword,
-  cb
-) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch);
-  });
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
+
 
 module.exports = mongoose.model("User", UserSchema);
